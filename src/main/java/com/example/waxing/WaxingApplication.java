f0813a7e3015639e8a2f618e8d278f329df5ed6b
yunjh99 <hans@bluesis.com>
2026-04-24 11:10:20 +0900

    first commit

---
+++ src/main/java/com/example/waxing/WaxingApplication.java
@@ -0,0 +1,13 @@
+package com.example.waxing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaxingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaxingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/waxing/WaxingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.waxing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaxingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
